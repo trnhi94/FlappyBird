1c99dfea594f8f3991c2836c7a735ede4c512751
Nhi Tran <trnhi1094@gmail.com>
2023-03-17 17:20:06 +0700

    BirdController

---
+++ Assets/_Game/Scripts/Handler/Collide2D.cs
@@ -0,0 +1,51 @@
+using System.Windows;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide2D : MonoBehaviour
+{
+    [SerializeField] private Vector2 size = Vector2.one;
+    public float Top => transform.position.y + (size.y / 2f);
+    public float Bottom => transform.position.y - (size.y / 2f);
+    public float Left => transform.position.x - (size.x / 2f);
+    public float Right => transform.position.x + (size.x / 2f);
+
+    private static List<Collide2D> allCollides = new List<Collide2D>();
+
+    private void Awake()
+    {
+        allCollides.Add(this);
+    }
+
+    private void Update()
+    {
+        if(GetCollisions().Count > 0)
+        {
+            Debug.Log("Collision!!");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+    private void OnDestroy()
+    {
+        allCollides.Remove(this);
+    }
+
+    public List<Collide2D> GetCollisions()
+    {
+        return allCollides.FindAll((collide) =>
+        {
+            if (collide == this)
+                return false;
+
+            return !(Right <= collide.Left || Left >= collide.Right || Top <= collide.Bottom || Bottom >= collide.Top);
+        });
+    }
+
+
+}
--- Assets/_Game/Scripts/Handler/GameObject/Bird.cs
+++ Assets/_Game/Scripts/Handler/GameObject/Bird.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     private Animator anim;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float gravity = 9.8f;
+    private float velocityY;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        velocityY -= gravity * Time.deltaTime;
+        transform.position += Vector3.up * velocityY * Time.deltaTime;
+
+        //if (transform.position.y <= 0)
+        //{
+        //    velocityY = 0;
+        //    transform.position = new Vector3(transform.position.x, 0, 0);
+        //}
+
+        if (Input.GetMouseButtonDown(0))
         {
             anim.SetTrigger("Flappy");
+            velocityY = jumpForce;
         }
     }
 }
